f09a536ccbd376495e68a44212eb2cd31c05ff63
Maksim Pernikau <mpernikau@gmail.com>
2022-05-15 18:18:10 +0300

    Removed unnecessary tests

--- test_login_page_VR.py
+++ test_login_page_VR.py
@@ -1,23 +1,14 @@
-from selenium import webdriver
-import time
-from selenium.webdriver.common.by import By
-from .pages.base_page import BasePage
-from .pages.login_page import LoginPage
+from pages.base_page import BasePage
 
-link = "https://staging.vr-smart-guide.de/login"
 
-class LoginPageTests():
-
-     def userCanLogin(browser, link):
-
-         page = BasePage(browser, link)
-         page.open()
-         login_page = LoginPage(browser, link)
-         login_page.personal_data_button_click()
-         login_page.fill_email_password_fields()
+def test_User_Can_Login(browser):
+    page = BasePage(browser, "https://staging.vr-smart-guide.de/login")
+    page.open()
+    page.personal_data_button_click()
+    page.fill_email_password_fields()
 
 
 
 
-# не забываем оставить пустую строку в конце файла
+
 
