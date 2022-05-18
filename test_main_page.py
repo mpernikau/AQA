51bec80e16fbe9a9a5378bd2b901f4c0e0fef94a
Maksim Pernikau <mpernikau@gmail.com>
2022-05-18 22:45:05 +0300

    Added new tests for proposal page

--- test_main_page.py
+++ test_main_page.py
@@ -13,10 +13,14 @@ class TestLoggedUser:
         page.fill_email_password_fields()
         time.sleep(1)
 
-    def test_user_is_logged_in(self, browser, timeout=15):
+    def test_user_can_enter_proposals_page(self, browser, timeout=15):
         page = MainPage(browser, "https://staging.vr-smart-guide.de")
         page.open()
         page.hover_menu()
         page.enter_outgoing_invoice_page()
         page.enter_proposal_page()
+        page.create_new_proposal()
+        page.filling_proposal_inputs_1()
+        page.scroll_page()
+        page.filling_proposal_inputs_2()
         time.sleep(3)
